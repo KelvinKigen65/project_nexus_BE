6a73044d0eb605414ec416ca5e393d94d69cbddc
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-08-10 13:32:26 +0300

    REST_FRAMEWORK updates

--- core/settings.py
+++ core/settings.py
@@ -122,6 +122,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # Add this line
+    ],
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
