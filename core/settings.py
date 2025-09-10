bd556808a70537f00a92b7a00bca8271717f80e6
Kelvin Kigen <Kelvinkigenkosgei@gmail.com>
2025-09-10 23:59:15 +0300

    products update

--- core/settings.py
+++ core/settings.py
@@ -75,7 +75,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # ✅ UNCOMMENTED for static file serving
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
